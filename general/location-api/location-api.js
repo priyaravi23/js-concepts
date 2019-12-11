0fbeb5a587e32914174c38539c80a65209b3cd4b
Priya Ravi <priyaravi23@gmail.com>
2019-12-10 17:40:30 -0800

    api-call

--- general/location-api/location-api.js
+++ general/location-api/location-api.js
@@ -4,11 +4,33 @@ document.addEventListener('readystatechange', function () {
        promise.then((res) => {
            if (res.statusText === 'OK') {
                res.json().then((data) => {
-                   console.log(data);
+
+                   const root = document.querySelector('.container');
+                   render(root, data)
                })
            }
        }, (err) => {
            console.log(err);
        });
+
+       const containers = document.querySelectorAll('.container');
+       console.log(`containers are: ${containers}`);
    }
 });
+
+function render(root, data) {
+    for (const key in data) {
+        let name = createDivElement(key, 'repo');
+        let value = createDivElement(data[key], 'name');
+
+        name.appendChild(value);
+        root.appendChild(name);
+    }
+}
+
+function createDivElement(str, className) {
+    let div = document.createElement('div');
+    div.setAttribute('class', className);
+    div.innerHTML = str;
+    return div;
+}
